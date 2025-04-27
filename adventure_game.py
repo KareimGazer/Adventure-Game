8f124c9c8b8b5b049e163f17c1b7107fbf641fec
Kareim Tarek <kareimgazar1990@gmail.com>
2025-04-27 19:27:58 +0300

    batman

---
+++ adventure_game.py
@@ -0,0 +1,9 @@
+
+def print_pause(message, delay=2):
+    import time
+    print(message)
+    time.sleep(delay)
+
+print_pause("welcome to the mysterious land of Pythonia!")
+print_pause("Rumors say that a wicked troll is frightening the villagers.")
+print_pause("but you are the super hero who will save the day!")
